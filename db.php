d7236d79800a2569664e35d7084b460d56dc436f
RoyLin <orangeskyonline@gmail.com>
2024-11-26 14:42:08 +0800

    在類別內建立取出單筆和多筆的兩個共用函式

--- db.php
+++ db.php
@@ -8,20 +8,66 @@ class DB{
         $this->table=$table;
         $this->pdo=new PDO($this->dsn,'root','');
     }
-function q($sql){
+   /* 撈出全部資料
+   1. 整張資料表
+   2. 有條件
+   3. 其它SQL功能
+   
+   */
+    function all(...$arg){
+        $sql="SELECT * FROM $this->table ";
+        if(!empty($arg[0])){
+            if(is_array($arg[0])){
+
+                $where=$this->a2s($arg[0]);
+                $sql=$sql . " WHERE ". join(" && ",$where);
+                
+            }else{
+                //$sql=$sql.$arg[0];
+                $sql .= $arg[0];
+            }
+        }
+            if(!empty($arg[1])){
+                $sql=$sql . $arg[1];
+            }
+
+        return $this->fetchAll($sql);
+    }
+
+
+    // 把陣列轉成條件字串陣列
+    function a2s($array){
+        $tmp=[];
+        foreach($array as $key => $value){
+            $tmp[]="`$key`='$value'";
+        }
+        return $tmp;
+    }
+function fetchOne($sql){
+    //echo $sql
+    return $this->pdo->query($sql)->fetch();
+}
+function fetchAll($sql){
+    // echo $sql;
     return $this->pdo->query($sql)->fetchAll();
-}
 
 }
 
-function dd($array){
-    echo "<pre>";
-    print_r($array);
-    echo "</pre>";
 }
+
+
+
+/* function q($sql){
+    return $this->pdo->query($sql)->fetchAll();} */
+
+    function dd($array){
+        echo "<pre>";
+        print_r($array);
+        echo "</pre>";
+    }
 
 
 $DEPT=new DB('dept');
-$dept=$DEPT->q("SELECT * FROM dept");
+$dept=$DEPT->all(" Order by `id` desc");
 
 dd($dept);
